fc40922c238ee39427dbb7300ee1893af885d12c
habinkim <danny9643@naver.com>
2021-08-04 16:24:16 +0900

    수정

---
+++ user-service/src/main/java/com/example/userservice/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.example.userservice.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class UserDto {
+
+	private String email;
+	private String name;
+	private String pwd;
+	private String userId;
+	private LocalDate createdAt;
+
+	private String encryptedPwd;
+
+}
